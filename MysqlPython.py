9eefb687b93cc5c8f0fdbbb94854b30b7479d3c6
Loyisk <loyisk.yt@gmail.com>
2021-05-20 13:37:47 +0200

    UPDATED POINT.5

--- MysqlPython.py
+++ MysqlPython.py
@@ -8,6 +8,7 @@ from playsound import playsound
 import json
 from mysql.connector.errors import InterfaceError, ProgrammingError
 
+
 print("\n",
 "\033[1;32;40m███╗   ███╗██╗   ██╗███████╗ ██████╗ ██╗\n",     
 "\033[1;32;40m████╗ ████║╚██╗ ██╔╝██╔════╝██╔═══██╗██║\n",     
@@ -17,6 +18,8 @@ print("\n",
 "\033[1;32;40m╚═╝     ╚═╝   ╚═╝   ╚══════╝ ╚══▀▀═╝ ╚══════╝\n"
 "\033[1;31;40mScript made by kastor",
 "\n")
+#playsound("intro.wav")
+
 
 def abelardoAsc():
   
@@ -45,7 +48,33 @@ def abelardoAsc():
   time.sleep(9)
   mainMenu()
 
+def mainConnect():
+    file = open("database.txt","r")
+    fileContent = file.readlines()
+    passwordstr = str(fileContent[2])
+    
+    if len(passwordstr) == 1:
+      passwordstr = ("")
 
+    mainConnect.mydb = mysql.connector.connect(
+    host=fileContent[0],
+    user=fileContent[1],
+    password= passwordstr,
+    database=fileContent[3])
+    
+    mainConnect.mycursor = mainConnect.mydb.cursor()
+def returnToMainMenu():
+  answer = print("¿Do you want to return to main menu? y/n: ")
+  if answer == "y":
+    mainMenu()
+  elif answer == "n":
+    exitScript()
+  elif  answer == "" or answer != "y" or answer != "n":
+    print("Please enter a valid answer, y or n\n")
+    time.sleep(1)
+    returnToMainMenu()
+
+    
 def changeCredentials(): 
       file = open("database.txt","w")
       hostIp = input("Give a server ip: ")
@@ -139,12 +168,33 @@ def createTables():
             database=fileContent[3])
             
             mycursor = mydb.cursor()
-            query =  "CREATE TABLE " + table_name + """(name VARCHAR(255), address VARCHAR(255))"""
+            
+
+            query =  ("CREATE TABLE " + table_name + "(id int NOT NULL AUTO_INCREMENT, PRIMARY KEY (id));""") 
+            
+            try:
+              numberOfColumns = int(input("¿How many columns do you want?: "))
+            except ValueError:
+              print("You have to enter a valid number")
+              createTables()
             mycursor.execute(query)
-            print("table created")
-            break
+
+            while numberOfColumns > 0:
+              columnName = str(input("Name for the column number " + str(numberOfColumns) + ": "))
+              query2 = ("""ALTER TABLE """ + table_name + """ ADD COLUMN """ + columnName + """ VARCHAR(255);""")
+
+              mycursor.execute(query2)
+              
+              numberOfColumns= numberOfColumns-1
+              if numberOfColumns == 0:
+                print("\nThe table and columns were successfully created\n \nReturning to main menu...\n")
+                time.sleep(2)
+                mainMenu()
+                break 
+            
+            
         except ProgrammingError: 
-            print ("Table name already exists, choose another one")
+            print ("Something went wrong, trying again...")
             createTables()
             
   else:
@@ -153,7 +203,33 @@ def createTables():
 def createDabatase():
   print("Creating")
 def listTables():
-  print("listing")
+  mainConnect()
+  query = ("SHOW TABLES;")
+  mainConnect.mycursor.execute(query)
+  
+  def listTablesOptions():
+    choose = input("¿Do you want to see the tables slowly(1) or in a shot(2)? 1/2: ")
+    if choose == "1":
+      print("listing...\n")
+      time.sleep(2)
+      for x in mainConnect.mycursor:
+          time.sleep(.50)
+          print(x)
+        #TODO solve the for
+      returnToMainMenu()
+    elif choose == "2":
+      print("listing...\n")
+      time.sleep(2)
+
+      for x in mainConnect.mycursor:
+        print(x) 
+      returnToMainMenu() 
+    elif choose == "" or choose != "1" or choose != "2":   
+      print("Please give a valid answer 1 or 2")
+      listTablesOptions()
+
+  listTablesOptions()
+
 def exitScript():
   print("exiting...")
   time.sleep(1)
@@ -169,6 +245,7 @@ def exitToMenut():
 
 def mainMenu():
   print(" 1. To connect to a MYsql server\n",
+  
         "2. To create new databases\n",
         "3. To create new tables\n",
         "4. To list all tables\n",
@@ -178,18 +255,25 @@ def mainMenu():
 
 
   if option == 1:
+    playsound('click.wav')
     setConnection()
   elif option == 2:
+    playsound('click.wav')
     createDabatase()
   elif option == 3:
+    playsound('click.wav')
     createTables()
   elif option == 4:
+    playsound('click.wav')
     listTables()
   elif option == 5:
+    playsound('click.wav')
     checkCredentials()
   elif option == 6:
+    playsound('click.wav')
     exitScript()
   elif option == 666:
+    playsound('click.wav')
     abelardoAsc()
     
 def chooseAnOption():
